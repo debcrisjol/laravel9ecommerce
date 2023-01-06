d9d6e95eb12a26d12f6ee316f6a3998555510398
Cristina Danescu <103639236+debcrisjol@users.noreply.github.com>
2023-01-06 23:03:14 +0000

    .

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Livewire\HomeComponent;
-use App\Http\Livewire\ShopComponent;
+use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,10 +23,21 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
 Route::get('/',HomeComponent::class)->name('home.index');
 Route::get('/shop',ShopComponent::class)->name('shop');
 Route::get('/cart',CartComponent::class)->name('shop.cart');
 Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
 
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth') ->group(function(){
+    Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+});
+Route::middleware(['auth','authadmin']) ->group(function () {
+    Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+require __DIR__.'/auth.php';
